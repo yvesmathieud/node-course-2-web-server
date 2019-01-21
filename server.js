a51090208d03f8ce4901440c20de59e95ceb3c6f
Dathia Nzounkeu <yves.dathianzounkeu@cgi.com>
2019-01-21 11:06:14 -0500

    Add project page

--- server.js
+++ server.js
@@ -48,6 +48,11 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
 
 app.get('/bad', (req, res) => {
     res.send({
